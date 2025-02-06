52978ab57a31b3c38570d25d860ecc819f7b9ec9
ArKynn <david.f.mendes@proton.me>
2025-02-06 12:11:14 +0000

    updated scene

---
+++ Assets/PaddleDetachPizza.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class PaddleDetachPizza : MonoBehaviour
+{
+    
+    private XRSocketInteractor socketInteractor;
+    
+    void Start()
+    {
+        socketInteractor = GetComponent<XRSocketInteractor>();    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Oven") == false) return;
+
+        var ovenSocket = other.GetComponent<XRSocketInteractor>();
+        if(ovenSocket == null) return;
+
+        switch (socketInteractor.hasSelection)
+        {
+            case true when !ovenSocket.hasSelection:
+                socketInteractor.enabled = false;
+                break;
+            case false when ovenSocket.hasSelection:
+                ovenSocket.enabled = false;
+                break;
+        }
+    }
+}
