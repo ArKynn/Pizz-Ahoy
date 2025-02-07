2cbcbcfeae773665d7c670e8cb8d541db9f0f6d5
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2025-02-07 16:50:25 +0000

    Moved ingredient scripts into a folder. Begin implementing utility tools.

---
+++ Assets/Main Assets/Scripts/UtilityTool.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+[RequireComponent(typeof(XRGrabInteractable))]
+public class UtilityTool : MonoBehaviour
+{
+    protected XRGrabInteractable grabInteractable;
+
+    protected void Start()
+    {
+        grabInteractable.GetComponent<XRGrabInteractable>();
+        grabInteractable.activated.AddListener(ActivatedAction);
+        grabInteractable.deactivated.AddListener(DeactivatedAction);
+
+    }
+
+    protected virtual void ActivatedAction(ActivateEventArgs args)
+    {
+        Debug.Log("Activated");
+    }
+
+    protected virtual void DeactivatedAction(DeactivateEventArgs args)
+    {
+        Debug.Log("Deactivated");
+    }
+}
