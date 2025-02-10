a7eeed4d181d64d94e7c9bdf5508a449fb234869
ArKynn <david.f.mendes@proton.me>
2025-02-10 14:43:09 +0000

    Added order class and pizzabox class, created pizzaBox prefab

---
+++ Assets/Main Assets/Scripts/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Main_Assets.Scripts
+{
+    public class Order : MonoBehaviour
+    {
+        public Dictionary<Ingredient, int> order { get; private set; } = new Dictionary<Ingredient, int>();
+        private bool orderSet;
+        
+        public void SetOrder(Dictionary<Ingredient, int> newOrder)
+        {
+            if(orderSet) return;
+            
+            order = newOrder;
+            orderSet = true;
+        }
+    }
+}
---
+++ Assets/Main Assets/Scripts/PizzaBox.cs
@@ -0,0 +1,61 @@
+using System;
+using Main_Assets.Scripts;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class PizzaBox : MonoBehaviour
+{
+    [SerializeField] private XRSocketInteractor bottomBox;
+    [SerializeField] private XRSocketInteractor topBox;
+    private Animator _animator;
+    private bool _hasPizza;
+    private bool _hasOrder;
+    
+    public Pizza pizza {get; private set;}
+    public Pizza order {get; private set;}
+    
+    
+    private void Start()
+    {
+        bottomBox.selectEntered.AddListener(UpdateRequirements);
+        bottomBox.selectExited.AddListener(UpdateRequirements);
+        topBox.selectEntered.AddListener(UpdateRequirements);
+        topBox.selectExited.AddListener(UpdateRequirements);
+    }
+
+    private void UpdateRequirements(SelectEnterEventArgs args = null)
+    {
+        CheckHasPizza();
+        CheckHasOrder();
+        CheckCanCloseBox();
+    }
+
+    private void UpdateRequirements(SelectExitEventArgs args = null)
+    {
+        CheckHasPizza();
+        CheckHasOrder();
+        CheckCanCloseBox();
+    }
+
+    private void CheckHasPizza()
+    {
+        _hasPizza = bottomBox.hasSelection &&
+                    (pizza = bottomBox.interactablesSelected[0].transform.gameObject.GetComponent<Pizza>()) != null;
+    }
+
+    private void CheckHasOrder()
+    {
+        _hasOrder = topBox.hasSelection && 
+                    (order = topBox.interactablesSelected[0].transform.gameObject.GetComponent<Pizza>()) != null;
+    }
+
+    private void CheckCanCloseBox()
+    {
+        if(!_hasPizza && _hasOrder) return;
+        
+        _animator.SetTrigger("Close");
+        bottomBox.GetComponent<XRGrabInteractable>().interactionLayers = InteractionLayerMask.GetMask("SocketBox");
+    }
+}
