ea73f3fbf20c53a18cfc4b928ad94963aceb7e3d
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2025-02-13 18:45:30 +0000

    Add Tutorial Book prefab

---
+++ Assets/MainAssets/Scripts/TutorialBook.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class TutorialBook : UtilityTool
+{
+    [SerializeField] private CanvasGroup tutorialUI;
+    [SerializeField] private List<Sprite> tutorialImages;
+    [SerializeField] private Image image;
+    [SerializeField] private Button nextbutton;
+    [SerializeField] private Button prevbutton;
+
+    private UIManager uiManager;
+    private HashSet<XRBaseInteractor> interactors = new HashSet<XRBaseInteractor>();
+    private int currentPage;
+
+
+    protected override void Start()
+    {
+        base.Start();
+
+        uiManager = FindAnyObjectByType<UIManager>();
+
+        grabInteractable.selectEntered.AddListener(EnableUI);
+        grabInteractable.selectExited.AddListener(DisableUI);
+        nextbutton.onClick.AddListener(NextPage);
+        prevbutton.onClick.AddListener(PrevPage);
+
+        tutorialUI.alpha = 0;
+        tutorialUI.gameObject.SetActive(true);
+        currentPage = 0;
+        image.sprite = tutorialImages[0];
+        prevbutton.gameObject.SetActive(false);
+    }
+
+    private void NextPage()
+    {
+        currentPage++;
+
+        CheckPageButtons();
+
+        image.sprite = tutorialImages[currentPage];
+    }
+
+    private void PrevPage()
+    {
+        currentPage--;
+
+        CheckPageButtons();
+
+        image.sprite = tutorialImages[currentPage];
+    }
+    
+    private void EnableUI(SelectEnterEventArgs args)
+    {
+        interactors.Add((XRBaseInteractor)args.interactorObject);
+        Debug.Log($"book grabbed ({interactors.Count} interactors)");
+
+        if(interactors.Count > 0)
+            uiManager.StartCoroutine(uiManager.FadeInUI(tutorialUI));
+    }
+
+    private void DisableUI(SelectExitEventArgs args)
+    {
+        interactors.Remove((XRBaseInteractor)args.interactorObject);
+        Debug.Log($"book released ({interactors.Count} interactors)");
+
+        if(interactors.Count <= 0)
+        {
+            uiManager.StartCoroutine(uiManager.FadeOutUI(tutorialUI));
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    private void CheckPageButtons()
+    {
+        if(currentPage == tutorialImages.Count - 1)
+            nextbutton.gameObject.SetActive(false);
+        else
+            nextbutton.gameObject.SetActive(true);
+
+
+        if(currentPage == 0)
+            prevbutton.gameObject.SetActive(false);
+        else
+            prevbutton.gameObject.SetActive(true);
+    }
+}
